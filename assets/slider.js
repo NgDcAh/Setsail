05519f09c45bcd9f8102f4226bb0f1228010261d
DucAnh <ducanh@gmail.com>
2022-03-15 20:08:09 +0700

    update

--- assets/slider.js
+++ assets/slider.js
@@ -21,7 +21,24 @@ $(document).ready(function(){
         draggable:false,
         prevArrow:"<button type='button' class='slick-prev pull-left'></button>",
         nextArrow:"<button type='button' class='slick-next pull-right'></button>",
-        
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 740,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true
+            }
+          }
+        ]
     });
   });
 
@@ -33,7 +50,17 @@ $(document).ready(function(){
       slidesToScroll: 1,
       arrows: false,
       autoplay: true,
-      autoplaySpeed: 3000
+      autoplaySpeed: 3000,
+      responsive: [
+        {
+          breakpoint: 740,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true
+          }
+        }
+      ]
     });
   });
 
@@ -44,6 +71,24 @@ $(document).ready(function(){
       slidesToScroll: 1,
       arrows: false,
       autoplay: true,
-      autoplaySpeed: 3000
+      autoplaySpeed: 3000,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 740,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true
+          }
+        }
+      ]
     });
   });
